3112c4a7b7179d4c1ac527cbd977a5a34cae0d06
Yukio Takaseya <ytksy1218@gmail.com>
2017-03-31 16:16:26 +0900

    Add training

--- bin_search.py
+++ bin_search.py
@@ -1,12 +1,13 @@
+
 # 1.2
 #import math
 #
 #bin_search = math.log(240000,2) #240000から二分探索で任意の数字を特定する際の計算量
+#print(bin_search)
 #二分探索ではlog 2 n 個の要素を探す必要がある
 #ex) log 2 1024 = 10 だから二分方ならば10個の要素を探す必要がある
 #ソート済みの場合の話
 
-print(bin_search)
 
 def binary_search(list, item):
     low  = 0
@@ -29,3 +30,14 @@ my_list = [1,3,5,7,9]
 print(binary_search(my_list, 3))
 print(binary_search(my_list, -1))
 
+
+#練習問題
+#1.1
+import math
+print(math.log(128,2)) #128の要素から二分探索で任意の数字を特定する際の計算量
+    #=> 7
+
+#1.2
+import math
+print(math.log(256,2)) #256の要素から二分探索で任意の数字を特定する際の計算量
+    #=> 8
